263e7dd0bf9a29240e3400c1d0c38582adb2863d
henrik <>
2021-05-01 21:00:23 +0200

    Build release version

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 set -e
 gcc -fsanitize=undefined -g -Wpedantic -Wall -o chess -Iinclude -Iclient chess.c
+gcc -static -O2 -DNDEBUG -s -Wpedantic -Wall -o release_chess -Iinclude -Iclient chess.c
 echo "Server build success"
