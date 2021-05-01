d3dcb1f3807e625d14b985785b91af56f43e1add
henrik <>
2021-05-01 20:44:30 +0200

    Rename server->chess

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 set -e
-gcc -fsanitize=undefined -g -Wpedantic -Wall -o server -Iinclude -Iclient server.c
+gcc -fsanitize=undefined -g -Wpedantic -Wall -o chess -Iinclude -Iclient chess.c
 echo "Server build success"
