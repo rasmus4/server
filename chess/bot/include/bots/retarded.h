8326e47caf921c65c0ff90efe6df085aa24e3679
henrik <>
2021-07-09 15:50:25 +0200

    Start on support for multiple bots

--- chess/bot/include/bots/retarded.h
+++ chess/bot/include/bots/retarded.h
@@ -1,3 +1,7 @@
 #pragma once
 
+struct retarded {
+    int dummy;
+};
+
 static int retarded_makeMove(void *data, bool isHost, uint8_t *board, int32_t lastMoveFrom, int32_t lastMoveTo, int32_t *moveFrom, int32_t *moveTo);
--- chess/bot/src/main.c
+++ chess/bot/src/main.c
@@ -3,18 +3,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-static struct client client;
+static union {
+    struct retarded retarded;
+} main_botData;
+static struct client main_client;
 
 int main(int argc, char **argv) {
+    if (argc < 2) {
+        printf("Usage: %s BOT [ROOM_ID]\n", argv[0]);
+        return 1;
+    }
     int32_t roomId = -1;
-    if (argc > 1) roomId = atoi(argv[1]);
+    if (argc > 2) roomId = atoi(argv[2]);
 
     struct clientCallbacks callbacks;
-    clientCallbacks_create(&callbacks, NULL, retarded_makeMove);
+    if (strcmp(argv[1], "retarded") == 0) {
+        clientCallbacks_create(&callbacks, &main_botData.retarded, retarded_makeMove);
+    } else {
+        printf("Unknown bot: %s\n", argv[1]);
+        return 1;
+    }
 
-    client_create(&client, &callbacks);
-    int status = client_run(&client, "127.0.0.1", 8089, roomId);
+    client_create(&main_client, &callbacks);
+    int status = client_run(&main_client, "127.0.0.1", 8089, roomId);
     printf("Status: %d\n", status);
     return 0;
 }
