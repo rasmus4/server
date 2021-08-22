bc0cab8d713ddfe224ab4d2ff49155e7eb7f2aa3
henrik <>
2021-08-22 12:10:19 +0200

    Use gcc again

--- htmlPacker/build.sh
+++ htmlPacker/build.sh
@@ -1,2 +1,2 @@
 set -e
-clang -fsanitize=undefined -fsanitize=address -g -Wall -Wpedantic -Wextra -Wconversion -o htmlPacker htmlPacker.c
+gcc -fsanitize=undefined -fsanitize=address -g -Wall -Wpedantic -Wextra -Wconversion -o htmlPacker htmlPacker.c
